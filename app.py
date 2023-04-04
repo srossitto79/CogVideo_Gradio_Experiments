833f71eda895cbfad671e77724af0d51463f9dae
zhu <yufengzhu@users.noreply.huggingface.co>
2023-04-04 10:21:01 +0000

    Upload app.py
    debug

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def post(
                     'image_prompt': encoded_img
                     })
     r = requests.post(url, data, headers=headers)
-    print(r)
+    print(r.json())
     print('请求完毕...')
     translated_text = r.json()['data']['translated_text']
     result_video = r.json()['data']['result_video']
