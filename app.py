2e8cf5fd1bc6b03a1bfd5581b2619025c2c9ef0a
zR <2448370773@qq.com>
2024-08-05 15:58:14 +0800

    init

--- app.py
+++ app.py
@@ -1,138 +1,188 @@
-#!/usr/bin/env python
-
-from __future__ import annotations
-
+import os
 import gradio as gr
+import torch
+from diffusers import CogVideoXPipeline
+from diffusers.utils import export_to_video
+from datetime import datetime
+from openai import OpenAI
+import spaces
+import moviepy.editor as mp
 
-# from model import AppModel
+dtype = torch.float16
+device = "cuda" if torch.cuda.is_available() else "cpu"
+pipe = CogVideoXPipeline.from_pretrained("THUDM/CogVideoX-2b", torch_dtype=dtype).to(device)
 
-MAINTENANCE_NOTICE='Sorry, due to computing resources issues, this space is under maintenance, and will be restored as soon as possible. '
+sys_prompt = """You are part of a team of bots that creates videos. You work with an assistant bot that will draw anything you say in square brackets.
 
-DESCRIPTION = '''# <a href="https://github.com/THUDM/CogVideo">CogVideo</a>
-Currently, this Space only supports the first stage of the CogVideo pipeline due to hardware limitations.
-The model accepts only Chinese as input.
-By checking the "Translate to Chinese" checkbox, the results of English to Chinese translation with [this Space](https://huggingface.co/spaces/chinhon/translation_eng2ch) will be used as input.
-Since the translation model may mistranslate, you may want to use the translation results from other translation services.
-'''
-NOTES = 'This app is adapted from <a href="https://github.com/hysts/CogVideo_demo">https://github.com/hysts/CogVideo_demo</a>. It would be recommended to use the repo if you want to run the app yourself.'
-FOOTER = '<img id="visitor-badge" alt="visitor badge" src="https://visitor-badge.glitch.me/badge?page_id=THUDM.CogVideo" />'
+For example , outputting " a beautiful morning in the woods with the sun peaking through the trees " will trigger your partner bot to output an video of a forest morning , as described. You will be prompted by people looking to create detailed , amazing videos. The way to accomplish this is to take their short prompts and make them extremely detailed and descriptive.
+There are a few rules to follow:
 
-import json
-import requests
-import numpy as np
-import imageio.v2 as iio
-import base64
-import urllib.request 
+You will only ever output a single video description per user request.
 
-def post(
-        text,
-        translate,
-        seed,
-        only_first_stage,
-        image_prompt
-        ):
-    url = 'https://tianqi.aminer.cn/cogvideo/api/generate'
-    headers = {
-            "Content-Type": "application/json; charset=UTF-8",
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36",
-        }
-    if image_prompt:
-        with open(image_prompt, "rb") as image_file:
-            encoded_img = str(base64.b64encode(image_file.read()), encoding='utf-8')
-    else:
-        encoded_img = None
-    print('开始请求...')
-    data = json.dumps({'text': text,
-                    'translate': translate,
-                    'seed': seed,
-                    'only_first_stage': only_first_stage,
-                    'image_prompt': encoded_img
-                    })
-    r = requests.post(url, data, headers=headers)
-    print(r)
-    
-    print('请求完毕...')
-    # translated_text = r.json()['data']['translated_text']
-    frames = r.json()['data']['frames']
+When modifications are requested , you should not simply make the description longer . You should refactor the entire description to integrate the suggestions.
+Other times the user will not want modifications , but instead want a new image . In this case , you should ignore your previous conversation with the user.
 
-    result_video = ["" for i in range(len(frames))]
-    result_video[0] = "./temp1.mp4"
-    result_video[1] = "./temp2.mp4"
-    for i in range(len(result_video)):
-        url = frames[i]
-        result_video[i] = "./temp" + str(i) + ".mp4"
-        urllib.request.urlretrieve(url, result_video[i])
+Video descriptions must have the same num of words as examples below. Extra words will be ignored.
+"""
 
-    print('finished')
-    return result_video[0], result_video[1]
-    # return result_video[0], result_video[1], result_video[2], result_video[3]
 
-def main():
-    only_first_stage = True
-    # model = AppModel(only_first_stage)
+def convert_prompt(prompt: str, retry_times: int = 3) -> str:
+    if not os.environ.get("OPENAI_API_KEY"):
+        return prompt
+    client = OpenAI()
+    text = prompt.strip()
 
-    with gr.Blocks(css='style.css') as demo:
-        # gr.Markdown(MAINTENANCE_NOTICE)
-        
-        gr.Markdown(DESCRIPTION)
+    for i in range(retry_times):
+        response = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": sys_prompt},
+                {"role": "user",
+                 "content": 'Create an imaginative video descriptive caption or modify an earlier caption for the user input : "a girl is on the beach"'},
+                {"role": "assistant",
+                 "content": "A radiant woman stands on a deserted beach, arms outstretched, wearing a beige trench coat, white blouse, light blue jeans, and chic boots, against a backdrop of soft sky and sea. Moments later, she is seen mid-twirl, arms exuberant, with the lighting suggesting dawn or dusk. Then, she runs along the beach, her attire complemented by an off-white scarf and black ankle boots, the tranquil sea behind her. Finally, she holds a paper airplane, her pose reflecting joy and freedom, with the ocean's gentle waves and the sky's soft pastel hues enhancing the serene ambiance."},
+                {"role": "user",
+                 "content": 'Create an imaginative video descriptive caption or modify an earlier caption for the user input : "A man jogging on a football field"'},
+                {"role": "assistant",
+                 "content": "A determined man in athletic attire, including a blue long-sleeve shirt, black shorts, and blue socks, jogs around a snow-covered soccer field, showcasing his solitary exercise in a quiet, overcast setting. His long dreadlocks, focused expression, and the serene winter backdrop highlight his dedication to fitness. As he moves, his attire, consisting of a blue sports sweatshirt, black athletic pants, gloves, and sneakers, grips the snowy ground. He is seen running past a chain-link fence enclosing the playground area, with a basketball hoop and children's slide, suggesting a moment of solitary exercise amidst the empty field."},
+                {"role": "user",
+                 "content": 'Create an imaginative video descriptive caption or modify an earlier caption for the user input : " A woman is dancing, HD footage, close-up"'},
+                {"role": "assistant",
+                 "content": "A young woman with her hair in an updo and wearing a teal hoodie stands against a light backdrop, initially looking over her shoulder with a contemplative expression. She then confidently makes a subtle dance move, suggesting rhythm and movement. Next, she appears poised and focused, looking directly at the camera. Her expression shifts to one of introspection as she gazes downward slightly. Finally, she dances with confidence, her left hand over her heart, symbolizing a poignant moment, all while dressed in the same teal hoodie against a plain, light-colored background."},
+                {"role": "user",
+                 "content": f'Create an imaginative video descriptive caption or modify an earlier caption in ENGLISH for the user input: "{text}"'},
+            ],
+            model="glm-4-0520",
+            temperature=0.01,
+            top_p=0.7,
+            stream=False,
+            max_tokens=250,
+        )
+        if response.choices:
+            return response.choices[0].message.content
+    return prompt
 
-        with gr.Row():
+
+@spaces.GPU()
+def infer(
+        prompt: str,
+        num_inference_steps: int,
+        guidance_scale: float,
+        progress=gr.Progress(track_tqdm=True)
+):
+    torch.cuda.empty_cache()
+
+    prompt_embeds, _ = pipe.encode_prompt(
+        prompt=prompt,
+        negative_prompt=None,
+        do_classifier_free_guidance=True,
+        num_videos_per_prompt=1,
+        max_sequence_length=226,
+        device=device,
+        dtype=dtype,
+    )
+
+    video = pipe(
+        num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale,
+        prompt_embeds=prompt_embeds,
+        negative_prompt_embeds=torch.zeros_like(prompt_embeds),
+    ).frames[0]
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    video_path = f"./output/{timestamp}.mp4"
+    os.makedirs(os.path.dirname(video_path), exist_ok=True)
+    export_to_video(video, video_path)
+    return video_path
+
+
+def convert_to_gif(video_path):
+    clip = mp.VideoFileClip(video_path)
+    clip = clip.set_fps(8)
+    clip = clip.resize(height=240)
+    gif_path = video_path.replace('.mp4', '.gif')
+    clip.write_gif(gif_path, fps=8)
+    return gif_path
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("""
+           <div style="text-align: center; font-size: 32px; font-weight: bold; margin-bottom: 20px;">
+               CogVideoX-2B Huggingface Space🤗
+           </div>
+           <div style="text-align: center;">
+               <a href="https://huggingface.co/THUDM/CogVideoX-2b">🤗 Model Hub</a> |
+               <a href="https://github.com/THUDM/CogVideo">🌐 Github</a> 
+           </div>
+
+           <div style="text-align: center; font-size: 15px; font-weight: bold; color: red; margin-bottom: 20px;">
+            ⚠️ This demo is for academic research and experiential use only. 
+            Users should strictly adhere to local laws and ethics.
+            </div>
+           """)
+    with gr.Row():
+        with gr.Column():
+            prompt = gr.Textbox(label="Prompt (Less than 200 Words)", placeholder="Enter your prompt here", lines=5)
+            with gr.Row():
+                gr.Markdown(
+                    "✨Upon pressing the enhanced prompt button, we will use [GLM-4 Model](https://github.com/THUDM/GLM-4) to polish the prompt and overwrite the original one.")
+                enhance_button = gr.Button("✨ Enhance Prompt(Optional)")
+
             with gr.Column():
-                with gr.Group():
-                    text = gr.Textbox(label='Input Text')
-                    translate = gr.Checkbox(label='Translate to Chinese',
-                                            value=False)
-                    seed = gr.Slider(0,
-                                     100000,
-                                     step=1,
-                                     value=1234,
-                                     label='Seed')
-                    only_first_stage = gr.Checkbox(
-                        label='Only First Stage',
-                        value=only_first_stage,
-                        visible=not only_first_stage)
-                    image_prompt = gr.Image(type="filepath",
-                                            label="Image Prompt",
-                                            value=None)
-                    run_button = gr.Button('Run')
+                gr.Markdown("**Optional Parameters** (default values are recommended)")
+                with gr.Row():
+                    num_inference_steps = gr.Number(label="Inference Steps", value=50)
+                    guidance_scale = gr.Number(label="Guidance Scale", value=6.0)
+                generate_button = gr.Button("🎬 Generate Video")
 
-            with gr.Column():
-                with gr.Group():
-                    #translated_text = gr.Textbox(label='Translated Text')
-                    with gr.Tabs():
-                        with gr.TabItem('Output (Video)'):
-                            result_video1 = gr.Video(show_label=False)
-                            result_video2 = gr.Video(show_label=False)
-                            # result_video3 = gr.Video(show_label=False)
-                            # result_video4 = gr.Video(show_label=False)
+        with gr.Column():
+            video_output = gr.Video(label="CogVideoX Generate Video", width=720, height=480)
+            with gr.Row():
+                download_video_button = gr.File(label="📥 Download Video", visible=False)
+                download_gif_button = gr.File(label="📥 Download GIF", visible=False)
 
 
+    def generate(prompt, num_inference_steps, guidance_scale, progress=gr.Progress(track_tqdm=True)):
+        video_path = infer(prompt, num_inference_steps, guidance_scale, progress=progress)
+        video_update = gr.update(visible=True, value=video_path)
 
-        # examples = gr.Examples(
-        #     examples=[['骑滑板的皮卡丘', False, 1234, True,None],
-        #               ['a cat playing chess', True, 1253, True,None]],
-        #     fn=model.run_with_translation,
-        #     inputs=[text, translate, seed, only_first_stage,image_prompt],
-        #     outputs=[translated_text, result_video],
-        #     cache_examples=True)
+        gif_path = convert_to_gif(video_path)
+        gif_update = gr.update(visible=True, value=gif_path)
 
-        gr.Markdown(NOTES)
-        gr.Markdown(FOOTER)
-        print(gr.__version__)
-        run_button.click(fn=post,
-                         inputs=[
-                             text,
-                             translate,
-                             seed,
-                             only_first_stage,
-                             image_prompt
-                         ],
-                         outputs=[result_video1, result_video2])
-                         # outputs=[result_video1, result_video2, result_video3, result_video4])
-        print(gr.__version__)
-    demo.queue(concurrency_count=6)
-    demo.launch()
+        return video_path, video_update, gif_update
 
 
-if __name__ == '__main__':
-    main()
+    def enhance_prompt_func(prompt):
+        return convert_prompt(prompt, retry_times=1)
+
+
+    generate_button.click(
+        generate,
+        inputs=[prompt, num_inference_steps, guidance_scale],
+        outputs=[video_output, download_video_button, download_gif_button]
+    )
+
+    enhance_button.click(
+        enhance_prompt_func,
+        inputs=[prompt],
+        outputs=[prompt]
+    )
+
+
+    def enhance_prompt_func(prompt):
+        return convert_prompt(prompt, retry_times=1)
+
+
+    generate_button.click(
+        generate,
+        inputs=[prompt, num_inference_steps, guidance_scale],
+        outputs=[video_output, download_video_button, download_gif_button]
+    )
+
+    enhance_button.click(
+        enhance_prompt_func,
+        inputs=[prompt],
+        outputs=[prompt]
+    )
+
+if __name__ == "__main__":
+    demo.launch(server_name="127.0.0.1", server_port=7870, share=True)
